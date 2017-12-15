c94406521dc2af4b2d552154e2e027cd63ecd71f
MatGod <mat_gord@mail.ru>
2017-12-15 02:35:06 +0200

    Analyse description

--- Site_2/main.js
+++ Site_2/main.js
@@ -1,28 +1,38 @@
-//Читаемый символ
-var ch;
-//Строка входных данных
-var str;
-//Счетчик позиций
-var pos;
+var text, ch, pos;
 
-function read() {
-
-    if (pos == str.length)
-        return false;
-
-    ch = str[pos];
-    pos++;
-    return true;
+function parseThis() {
+    var TextBox = document.getElementById('text');
+    text = TextBox.value;
+    ch = text[0];
+    pos = 0;
+    if (parseF() && BktBalance()) {
+        alert("Выражение корректно!");
+    }
+    else {
+        alert("Неверное выражение...");
+    }
 }
 
+function isLiteral() {
+    var lit = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
+    ];
+    for (var i = 0; i < lit.length; i++) {
+        if (ch == lit[i]) {
+            read();
+            return true;
+        }
+    }
+    return false;
+}
 
-function isVariable() {
-    var Variables = ['a' , 'b' , 'c' , 'd' , 'e' , 'f' , 'g' , 'h' , 'i' , 'j' , 'k' , 'l' ,'m' ,
-        'n' , 'o' , 'p' , 'q' , 'r' , 's' , 't' , 'u' , 'v' , 'w' , 'x' , 'y' , 'z'];
-
-    for (var i = 0 ; i < Variables.length ; i++ )
-        if (ch == Variables[i]) return true;
-
+function isLiteralWithoutRead() {
+    var lit = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
+    ];
+    for (var i = 0; i < lit.length; i++) {
+        if (ch == lit[i]) {
+            return true;
+        }
+    }
     return false;
 }
 
@@ -31,29 +41,33 @@ function isSign() {
         read();
         return true;
     }
-    if (isVariable() || ch == '(') {
+    else if (ch == '(' || isLiteralWithoutRead()) {
         return true;
     }
     return false;
 }
 
-function parseStart() {
-    if (ch == '(') return true;
-    else if (isVariable()) {
-        if (read()) {
-            if (isSign() && parseStart()) return true;
-        }
-        else return true;
+function read() {
+    if (pos < text.length) {
+        pos++;
+        ch = text[pos];
     }
-    return false;
+}
+
+function parseF() {
+    return (parseStart() && parseEnd());
+}
+
+function parseStart() {
+    if (isLiteral()) return parseEnd();
+    else return (parseBkt() && parseEnd());
 }
 
 function parseBkt() {
-    if (ch == '(') {
+    if (ch == "(") {
         read();
-        if (parseF()) {
-            read();
-            if (ch == ')') {
+        if (parseStart() && parseEnd()) {
+            if (ch == ")") {
                 read();
                 return true;
             }
@@ -63,40 +77,17 @@ function parseBkt() {
 }
 
 function parseEnd() {
-    if (isSign()) {
-        read();
-        if (isSign()) {
-            read();
-            if (parseF()) return true;
-        }
-    }
+    if (ch == ")" || ch == undefined) return true;
+    else if (isSign()) return parseStart();
     return false;
 }
 
-function parseF() {
-    var answer = true;
-    if (isVariable()) answer = parseStart();
-    if (ch) {
-        if (answer)
-            if (parseBkt())
-                if (isSign())
-                    if (parseEnd()) return true;
+function BktBalance() {
+    var balance = 0;
+    for (var i = 0; i < text.length; i++) {
+        if (text[i] == "(") balance++;
+        if (text[i] == ")") balance--;
     }
-    else return true;
-    return false;
-}
-
-function parseThis() {
-
-    pos = 0;
-    ch = 0;
-    str = "";
-
-    var text = document.getElementById('txt');
-    str = text.value.toLowerCase();
-    var answer = document.getElementById('answer');
-
-    read(ch);
-    if (parseF()) answer.innerHTML = "ВЕРНОЕ выражение!";
-    else answer.innerHTML = "Некорректное выражение!";
+    if (balance == 0) return true;
+    else return false;
 }
